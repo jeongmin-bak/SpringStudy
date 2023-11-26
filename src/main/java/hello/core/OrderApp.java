e169fb7de1d4e455057501d75b576019ebe1a60d
jungmin.park <pjm9673@gmail.com>
2023-11-26 23:25:23 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderServiceImpl;
+import hello.core.order.Orderservice;
+
+public class OrderApp {
+	public static void main(String[] args) {
+		MemberService memberService = new MemberServiceImpl();
+		Orderservice orderservice = new OrderServiceImpl();
+
+		Long memberId = 1L;
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Order order = orderservice.createOrder(memberId, "itemA", 10000);
+		System.out.println("order = " + order.toString());
+	}
+}
