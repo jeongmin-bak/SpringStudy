2acd19cfc59ada6653dac576b60d689b3fa1938b
jeongmin-bak <jmpark@didim365.com>
2024-09-30 18:58:39 +0900

    싱글톤 방식의 주의점

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 	@Test
@@ -40,5 +42,22 @@ public class SingletonTest {
 		//same ==
 		//equal
 
+	}
+
+	@Test
+	@DisplayName("스프링 컨테이너와 싱글톤")
+	void springContainer(){
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		//1. 조회: 호출할 때 마다 객체를 생성
+		MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+		//1. 조회: 호출할 때 마다 객체를 생성
+		MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+		//참조값이 다른 것을 확인
+		System.out.println("memberService1 = " + memberService1);
+		System.out.println("memberService2 = " + memberService2);
+
+		//memberService != memberService2
+		Assertions.assertThat(memberService1).isSameAs(memberService2);
 	}
 }
