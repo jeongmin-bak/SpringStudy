96f9f950805e78061dbf17fe508a03bd45c70304
jeongmin-bak <jmpark@didim365.com>
2024-10-01 19:05:31 +0900

    Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,34 @@ import hello.core.order.OrderService;
 
 @Configuration // 설정정보담당하는 어노테이션
 public class AppConfig {
+	//@Bean memberService -> new MemoryMemberRepository()
+	//@Bean orderService -> new MemoryMemberRepository()
+
+	// call AppConfig.memberService
+	// call AppConfig.memberRepository
+	// call AppConfig.memberRepository
+	// call AppConfig.orderService
+	// call AppConfig.memberRepository
+
+	//call AppConfig.memberService
+	//call AppConfig.memberRepository
+	//call AppConfig.orderService
+
 	@Bean
 	public MemberService memberService(){
+		System.out.println("call AppConfig.memberService");
 		return new MemberServiceImpl(memberRepository());
 	}
 
 	@Bean
 	public MemberRepository memberRepository(){
+		System.out.println("call AppConfig.memberRepository");
 		return new MemoryMemberRepository();
 	}
 
 	@Bean
 	public OrderService orderService(){
+		System.out.println("call AppConfig.orderService");
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
